65b6950ddb0e6bdb41b082f5e7ae664a2d46264e
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-06-14 11:00:20 +0700

    perbaikan antrian

--- resources/views/bpjs/antrian/dashboard_tanggal_index.blade.php
+++ resources/views/bpjs/antrian/dashboard_tanggal_index.blade.php
@@ -24,34 +24,44 @@
             </x-adminlte-card>
             <x-adminlte-card title="Data Task ID Antrian" theme="secondary" collapsible>
                 @php
-                    $heads = ['Poliklinik', 'Total Antrian', '1', '2', '3', '4', '5', '6', '7'];
+                    $heads = ['Poliklinik', 'Total Antrian', 'Checkin', 'Daftar', 'Tunggu Poli', 'Layan Poli', 'Tunggu Farmasi', 'Proses Farmasi', 'Selesai'];
+                    $config = ['paging' => false];
+
                 @endphp
-                <x-adminlte-datatable id="table2" class="text-xs" :heads="$heads" hoverable bordered compressed>
+                <x-adminlte-datatable id="table2" class="text-xs" :heads="$heads" :config="$config" hoverable bordered
+                    compressed>
                     @isset($antrians)
                         @foreach ($antrians->groupBy('namapoli') as $key => $item)
                             <tr>
                                 <td>{{ $key }}</td>
                                 <td>{{ $item->sum('jumlah_antrean') }}</td>
-                                <td>{{ round($item->sum('avg_waktu_task1') / 60 / $item->sum('jumlah_antrean')) }} menit</td>
-                                <td>{{ round($item->sum('avg_waktu_task2') / 60 / $item->sum('jumlah_antrean')) }} menit</td>
-                                <td>{{ round($item->sum('avg_waktu_task3') / 60 / $item->sum('jumlah_antrean')) }} menit</td>
-                                <td>{{ round($item->sum('avg_waktu_task4') / 60 / $item->sum('jumlah_antrean')) }} menit</td>
-                                <td>{{ round($item->sum('avg_waktu_task5') / 60 / $item->sum('jumlah_antrean')) }} menit</td>
-                                <td>{{ round($item->sum('avg_waktu_task6') / 60 / $item->sum('jumlah_antrean')) }} menit</td>
-                                <td>{{ round($item->sum('avg_waktu_task7') / 60 / $item->sum('jumlah_antrean')) }} menit</td>
+                                <td>{{ round($item->sum('avg_waktu_task1') / 60 / $item->count()) }} menit</td>
+                                <td>{{ round($item->sum('avg_waktu_task2') / 60 / $item->count()) }} menit</td>
+                                <td>{{ round($item->sum('avg_waktu_task3') / 60 / $item->count()) }} menit</td>
+                                <td>{{ round($item->sum('avg_waktu_task4') / 60 / $item->count()) }} menit</td>
+                                <td>{{ round($item->sum('avg_waktu_task5') / 60 / $item->count()) }} menit</td>
+                                <td>{{ round($item->sum('avg_waktu_task6') / 60 / $item->count()) }} menit</td>
+                                <td>{{ round($item->sum('avg_waktu_task7') / 60 / $item->count()) }} menit</td>
                             </tr>
                         @endforeach
                         <tfoot>
                             <tr>
                                 <th>Total</th>
                                 <th>{{ $antrians->sum('jumlah_antrean') }}</th>
-                                <td>{{ round($antrians->sum('avg_waktu_task1') / 60 / $antrians->sum('jumlah_antrean')) }} menit</td>
-                                <td>{{ round($antrians->sum('avg_waktu_task2') / 60 / $antrians->sum('jumlah_antrean')) }} menit</td>
-                                <td>{{ round($antrians->sum('avg_waktu_task3') / 60 / $antrians->sum('jumlah_antrean')) }} menit</td>
-                                <td>{{ round($antrians->sum('avg_waktu_task4') / 60 / $antrians->sum('jumlah_antrean')) }} menit</td>
-                                <td>{{ round($antrians->sum('avg_waktu_task5') / 60 / $antrians->sum('jumlah_antrean')) }} menit</td>
-                                <td>{{ round($antrians->sum('avg_waktu_task6') / 60 / $antrians->sum('jumlah_antrean')) }} menit</td>
-                                <td>{{ round($antrians->sum('avg_waktu_task7') / 60 / $antrians->sum('jumlah_antrean')) }} menit</td>
+                                <td>{{ round($antrians->sum('avg_waktu_task1') / 60 / $antrians->count()) }}
+                                    menit</td>
+                                <td>{{ round($antrians->sum('avg_waktu_task2') / 60 / $antrians->count()) }}
+                                    menit</td>
+                                <td>{{ round($antrians->sum('avg_waktu_task3') / 60 / $antrians->count()) }}
+                                    menit</td>
+                                <td>{{ round($antrians->sum('avg_waktu_task4') / 60 / $antrians->count()) }}
+                                    menit</td>
+                                <td>{{ round($antrians->sum('avg_waktu_task5') / 60 / $antrians->count()) }}
+                                    menit</td>
+                                <td>{{ round($antrians->sum('avg_waktu_task6') / 60 / $antrians->count()) }}
+                                    menit</td>
+                                <td>{{ round($antrians->sum('avg_waktu_task7') / 60 / $antrians->count()) }}
+                                    menit</td>
                             </tr>
                         </tfoot>
                     @endisset
