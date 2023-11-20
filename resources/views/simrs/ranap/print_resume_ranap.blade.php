df0757c244acaea653de9a8c0b8364c7b50fd2d6
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-11-20 09:49:46 +0700

    printer

--- resources/views/simrs/ranap/print_resume_ranap.blade.php
+++ resources/views/simrs/ranap/print_resume_ranap.blade.php
@@ -18,7 +18,7 @@
                         <div class="col-md-6  border border-dark">
                             <b>PEMERINTAHAN KABUPATEN CIREBON</b><br>
                             <b>RUMAH SAKIT UMUM DAERAH WALED</b><br>
-                            Jl. Prabu Kian Santang No. 4 Kec. Waled Kab. Cirebon Jawa Barat 45151<br>
+                            Jl. Prabu Kian Santang No. 4 Kab. Cirebon Jawa Barat 45151<br>
                             www.rsudwaled.id - 0823 1169 6919 - (0231) 8850943
                         </div>
                         <div class="col-md-4  border border-dark">
@@ -37,11 +37,85 @@
                         <div class="col-md-12 border border-dark text-center bg-warning">
                             <b>Ringkasan Pasien Pulang</b>
                         </div>
-                        <div class="col-md-4 border border-dark">
-
+                        <div class="col-md-6 border border-dark">
+                            <b>Kunjungan : </b> {{ $kunjungan->counter }} / {{ $kunjungan->kode_kunjungan }} <br>
+                            <b>Tanggal Masuk : </b> {{ $kunjungan->tgl_masuk ?? '-' }} <br>
+                            <b>Tanggal Keluar : </b> {{ $kunjungan->tgl_keluar ?? '-' }} <br>
+                            <b>Lama Rawat : </b> {{ $kunjungan->tgl_keluar ?? '-' }} <br><br>
+                            <dl>
+                                <dt>Ringkasan Perawatan :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->catatan_laboratorium ?? '-' }}</pre>
+                                </dd>
+                                <dt>Riwayat Penyakit :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->catatan_radiologi ?? '-' }}</pre>
+                                </dd>
+                                <dt>Indikasi Rawat Inap :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->catatan_penunjang ?? '-' }}</pre>
+                                </dd>
+                                <dt>Pemeriksaan Fisik :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->hasil_konsultasi ?? '-' }}</pre>
+                                </dd>
+                            </dl>
                         </div>
-                        <div class="col-md-8  border border-dark">
-
+                        <div class="col-md-6  border border-dark">
+                            <dl>
+                                <dt>Catatan Laboratorium :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->catatan_laboratorium ?? '-' }}</pre>
+                                </dd>
+                                <dt>Catatan Radiologi :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->catatan_radiologi ?? '-' }}</pre>
+                                </dd>
+                                <dt>Catatan Penunjang Lainnya :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->catatan_penunjang ?? '-' }}</pre>
+                                </dd>
+                                <dt>Hasil Konsultasi :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->hasil_konsultasi ?? '-' }}</pre>
+                                </dd>
+                            </dl>
+                        </div>
+                        <div class="col-md-6  border border-dark">
+                            <dl>
+                                <dt>Diagnosa Masuk :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->diagnosa_masuk ?? '-' }}</pre>
+                                </dd>
+                                <dt>Diagnosa Utama :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->diagnosa_utama ?? '-' }}</pre>
+                                </dd>
+                                <dt>Diagnosa Sekunder :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->diagnosa_sekunder ?? '-' }}</pre>
+                                </dd>
+                                <dt>Diagnosa ICD-10 :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->diagnosa_icd10 ?? '-' }}</pre>
+                                </dd>
+                                <dt>Kompikasi :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->komplikasi ?? '-' }}</pre>
+                                </dd>
+                            </dl>
+                        </div>
+                        <div class="col-md-6  border border-dark">
+                            <dl>
+                                <dt>Tindakan Operasi :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->tindakan ?? '-' }}</pre>
+                                </dd>
+                                <dt>Tindakan ICD-10 :</dt>
+                                <dd>
+                                    <pre>{{ $kunjungan->erm_ranap->tindakan_icd9 ?? '-' }}</pre>
+                                </dd>
+                            </dl>
                         </div>
                     </div>
                 </section>
@@ -52,6 +126,14 @@
     </div>
 @stop
 @section('css')
+    <style>
+        pre {
+            border: none;
+            outline: none;
+            padding: 0 !important;
+            font-size: 15px;
+        }
+    </style>
     <style type="text/css" media="print">
         hr {
             color: #333333 !important;
--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -26,10 +26,8 @@
     @if(!config('adminlte.enabled_laravel_mix'))
         <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
         <link rel="stylesheet" href="{{ asset('vendor/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
-
         {{-- Configured Stylesheets --}}
         @include('adminlte::plugins', ['type' => 'css'])
-
         <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
         {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic"> --}}
     @else
