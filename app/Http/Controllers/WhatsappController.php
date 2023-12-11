5907de9de0977599e0ff41ada65fe4d9cfe39987
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-12-11 09:44:22 +0700

    perbaikan

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -9,12 +9,6 @@ use RealRashid\SweetAlert\Facades\Alert;
 class WhatsappController extends Controller
 {
     public $hari = ["MINGGU", "SENIN", "SELASA", "RABU", "KAMIS", "JUMAT", "SABTU"];
-    public function test(Request $request)
-    {
-        $request['message']  = "DAFTAR KONTROL_1018R0011222K001074#504#2022-12-06";
-        $request['number'] = '6289529909036@c.us';
-        $sk = $this->callback($request);
-    }
     public function whatsapp(Request $request)
     {
         $request['number'] = "089529909036";
@@ -128,15 +122,7 @@ class WhatsappController extends Controller
         $pesan = strtoupper($request->message);
         switch ($pesan) {
             default:
-                // $request['fileurl'] = asset('rsudwaled/daftar.jpg');
-                // $request['caption'] = "Web SIRAMAH-RS Waled";
-                // $this->send_image($request);
                 $request['message'] = "Layanan pendaftaran rawat jalan RSUD Waled dapat melalui dua aplikasi beriku \n\n1. Web SIRAMAH-RS Waled : https://siramah.rsudwaled.id\n\n2. Aplikasi JKN : https://play.google.com/store/apps/details?id=app.bpjs.mobile";
-                // return $this->send_message($request);
-                // $this->send_message($request);
-                // $sholawat = "اَللّٰهُمَّ صَلِّ عَلٰى سَيِّدِنَا مُحَمَّدٍ، طِبِّ الْقُلُوْبِ وَدَوَائِهَا، وَعَافِيَةِ الْاَبْدَانِ وَشِفَائِهَا، وَنُوْرِ الْاَبْصَارِ وَضِيَائِهَا، وَعَلٰى اٰلِهِ وَصَحْبِهِ وَسَلِّمْ";
-                // $request['message'] = $sholawat;
-                // $request['number'] = '6289529909036@c.us';
                 return $this->send_message($request);
                 break;
         }
