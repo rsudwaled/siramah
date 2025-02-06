e457e1bc66f916be6dc8fd08d843627448df70d1
marwandhiaurrahman@gmail.com <marwandhiaurrahman@gmail.com>
2025-02-06 10:08:48 +0700

    export

--- app/Exports/MonitoringWaktuAntrianExport.php
+++ app/Exports/MonitoringWaktuAntrianExport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Models\Antrian;
+use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
@@ -29,6 +30,7 @@ class MonitoringWaktuAntrianExport implements FromCollection, WithHeadings
                 'nama' => $antrian->nama,
                 'nomorkartu' => $antrian->nomorkartu,
                 'namapoli' => $antrian->namapoli,
+                'namadokter' => $antrian->kunjungan?->dokter?->nama_paramedis,
                 'taskid3' => $antrian->taskid3,
                 'tanggalassemen' => $antrian->kunjungan?->assesmen_perawat?->tanggalassemen,
                 'taskid4' => $antrian->taskid4,
@@ -36,6 +38,7 @@ class MonitoringWaktuAntrianExport implements FromCollection, WithHeadings
                 'taskid5' => $antrian->taskid5,
                 'taskid6' => $antrian->kunjungan?->order_obat_header?->tgl_entry,
                 'taskid7' => $antrian->taskid7,
+                'waktupelayanan' => Carbon::parse($antrian->taskid3)->diff(Carbon::parse($antrian->taskid7))->format('%H:%I:%S'),
             ];
         });
         return $antrians;
@@ -48,13 +51,14 @@ class MonitoringWaktuAntrianExport implements FromCollection, WithHeadings
             'Nama Pasien',
             'Nomor BPJS',
             'Poliklinik',
+            'Dokter',
             'Checkin',
             'Anamnesis',
             'Periksa Dokter',
             'Selesai Dokter',
-
             'Penyiapan Resep',
             'Obat Diserahkan',
+            'Waktu Pelayanan',
         ];
     }
 }
