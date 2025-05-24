805c618d2e25036622aed225a9103970a94ac591
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2025-05-24 12:28:52 +0700

    perbaikan display jadwal

--- resources/views/livewire/antrian/display-jadwal-rajal.blade.php
+++ resources/views/livewire/antrian/display-jadwal-rajal.blade.php
@@ -1,30 +1,7 @@
-<div class="row">
-    <div class="col-md-12">
-        <div class="card">
-            <header class="bg-green text-white p-2">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="row">
-                                <img src="{{ asset('vendor/adminlte/dist/img/logo rsudwaled bulet.png') }}" height="90"
-                                    alt="">
-                                <div class="col">
-                                    <h1>RSUD Waled</h1>
-                                    <h4>Rumah Sakit Umum Daerah Waled</h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 text-md-end text-right">
-                            <h1>Jadwal Dokter Rawat Jalan</h1>
-                            <h4>{{ \Carbon\Carbon::now()->locale('id')->isoFormat('dddd, D MMMM Y') }}</h4>
-                        </div>
-                    </div>
-                </div>
-            </header>
-        </div>
-    </div>
+<div class="row p-1">
+    <x-header-anjungan-antrian />
     <div class="col-md-5">
-        <x-adminlte-card title="Antrian Hari Ini" class="mr-2" theme="primary">
+        <x-adminlte-card title="Antrian Hari Ini" class="mr-2" theme="success">
             <div class="row">
                 <div class="col-md-3">
                     <div class="text-center">
@@ -75,7 +52,7 @@
         </x-adminlte-card>
         <x-adminlte-card body-class="p-1" class="mr-2">
             <video width="100%" height="100%" controls autoplay muted loop>
-                <source src="{{ asset('bpjs/Video Sosialisasi Program Rehab 30sec.mp4') }}" type="video/mp4">
+                <source src="{{ asset('bpjs/0.mp4') }}" type="video/mp4">
                 Your browser does not support the video tag.
             </video>
         </x-adminlte-card>
@@ -87,7 +64,7 @@
                 <div class="carousel-inner">
                     @foreach ($jadwals->chunk(15) as $chunkKey => $chunk)
                         <div class="carousel-item {{ $chunkKey == 0 ? 'active' : '' }}">
-                            <x-adminlte-card title="Jadwal Dokter Hari Ini" theme="primary">
+                            <x-adminlte-card title="Jadwal Dokter Hari Ini" theme="success">
                                 <table class="table table-sm table-bordered">
                                     <thead>
                                         <tr>
